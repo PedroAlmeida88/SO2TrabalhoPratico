216739f8f99d34528926c39b3493802d3b3f2aec
José Pedro Sousa Almeida <80987531+PedroAlmeida88@users.noreply.github.com>
2023-05-13 13:47:32 +0100

    Add files via upload

---
+++ Operador/Game.c
@@ -0,0 +1,201 @@
+#include "Game.h"
+
+Game* createGame(int lanesNumber) {
+    Game* array = (Game*)malloc(lanesNumber * sizeof(Game));
+    if (array == NULL) {
+        printf("Erro ao alocar memoria");
+        return NULL;
+    }
+    return array;
+}
+
+void initGame(Game* game) {
+    for (int i = 0; i < TAM_LANE; i++) {     //Inicializar a primeira e a última via
+        game[0].estrada[i] = '_';
+        game[game->total_lanes - 1].estrada[i] = '-';
+    }
+    //game[0].velocity = 0;
+    //game[TOTAL_LANES - 1].velocity = 0;
+
+    for (int i = 1; i < game->total_lanes - 1; i++) { //Inicializar as vias restantes
+        for (int j = 0; j < TAM_LANE - 1; j++) {
+            game[i].estrada[j] = ' ';
+        }
+        //game[i].velocity = 5;
+    }
+    int posrand1;
+    int posrand2;
+    do {
+        posrand1 = randNum(1, TAM_LANE - 2);
+        posrand2 = randNum(1, TAM_LANE - 2);
+    } while (posrand1 == posrand2);
+    game[game->total_lanes - 1].estrada[posrand1] = 'S';
+    game[game->total_lanes - 1].estrada[posrand2] = 'S';
+}
+
+void show(Game* game) {
+    for (int i = 0; i < game->total_lanes; i++)
+    {
+
+        for (int j = 0; j < TAM_LANE - 1; j++)
+            _tprintf(TEXT("---"));
+        _tprintf(TEXT("\n"));
+
+        for (int j = 0; j < TAM_LANE - 1; j++)
+        {
+            if (game[i].estrada[j] == ' ')
+                _tprintf(TEXT("   "));
+            if (game[i].estrada[j] == '-')
+                _tprintf(TEXT("   "));
+            if (game[i].estrada[j] == '_')
+                _tprintf(TEXT("   "));
+            if (game[i].estrada[j] == 'C')
+                _tprintf(TEXT("8=D"));
+            if (game[i].estrada[j] == 'O')
+                _tprintf(TEXT(" O "));
+            if (game[i].estrada[j] == 'S')
+                _tprintf(TEXT(" S "));
+            //_tprintf(TEXT("%c"),game[i].estrada[j]);
+        }
+        _tprintf(TEXT("\n"));
+    }
+    for (int j = 0; j < TAM_LANE - 1; j++)
+        _tprintf(TEXT("---"));
+    _tprintf(TEXT("\n"));
+}
+
+DWORD WINAPI lanesFunction(LPVOID param) {
+    DadosLanesThread* dados = (DadosLanesThread*)param;
+    //int seed = *(int*)param;
+    int nCarros = 0;//num de carros na via
+    int starterVelocity = dados->velocity;
+    int velocity;
+    srand((unsigned int)time(NULL) ^ GetCurrentThreadId());
+    BOOL firstTime = TRUE;
+    int count = 0;
+    while (!dados->terminar) {
+        //int rand = randNum(0, 19);
+        //_tprintf(TEXT("Random number %d\n"), rand);
+        //esperamos que o mutex esteja livre
+        WaitForSingleObject(dados->hMutex, INFINITE);
+        if (firstTime) {
+            initCars(dados->game, dados->laneNumber, &nCarros);
+            velocity = starterVelocity;
+        }
+        else {
+            if (!dados->stop) {
+                moveCars(dados->currDirection, dados->game, dados->laneNumber, &nCarros);
+                velocity = dados->velocity;
+            }
+            else {
+                /*
+                LARGE_INTEGER dueTime;
+                // Define o tempo de espera para 5 segundos (5000 milissegundos)
+                dueTime.QuadPart = -50000000LL;
+                // Cria um timer que irá disparar depois de 5 segundos
+                HANDLE timer = CreateWaitableTimer(NULL, TRUE, NULL);
+                SetWaitableTimer(timer, &dueTime, 0, NULL, NULL, 0);
+
+                // Espera pelo timer
+                WaitForSingleObject(timer, INFINITE);
+                dados->stop = FALSE;
+                // Libera o timer
+                CloseHandle(timer);
+                */
+
+                count++;
+                if (count >= 3) {
+                    dados->stop = FALSE;
+                    count = 0;
+                }
+
+
+            }
+        }
+
+        system("cls");
+        show(dados->game);
+
+        _tprintf(TEXT("Thread/lane num %d\n\n\n"), dados->laneNumber);
+
+        ReleaseMutex(dados->hMutex);
+        Sleep(1000 - (10 * velocity));
+        firstTime = FALSE;
+
+    }
+
+}
+
+int randNum(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+void initCars(Game* game, int laneNumber, int* nCarros) {
+    for (int i = 0; i < TAM_LANE; i++) {
+        int flag = randNum(0, 4);//25% de chance de meter um carro numa pos
+        if (flag == 0) {
+            game[laneNumber].estrada[i] = 'C';
+            nCarros++;
+            if (*nCarros >= 8)
+                break;
+        }
+    }
+}
+
+void moveCars(int direction, Game* game, int laneNumber, int* nCarros) {
+
+    if (direction == 0) {
+        //direction: right
+        for (int i = 0; i < TAM_LANE; i++) {
+            if (game[laneNumber].estrada[i] == 'C') {
+                if (i != 0) {
+                    if (game[laneNumber].estrada[i - 1] != 'O') {
+                        game[laneNumber].estrada[i] = ' ';
+                        game[laneNumber].estrada[i - 1] = 'C';
+                    }
+                    else {
+                        game[laneNumber].estrada[i] = ' ';
+                    }
+                }
+                else {
+                    game[laneNumber].estrada[i] = ' ';
+                }
+            }
+
+        }
+        //25% de chance de spwanar um carr
+        if (*nCarros < 8) {
+            int flag = randNum(0, 4);
+            if (flag == 0 && game[laneNumber].estrada[TAM_LANE - 1] != 'O') {
+                game[laneNumber].estrada[TAM_LANE - 1] = 'C';
+            }
+        }
+
+    }
+    else {
+        //direction: left
+        for (int i = TAM_LANE - 1; i >= 0; i--) {
+            if (game[laneNumber].estrada[i] == 'C') {
+                if (i != TAM_LANE - 1) {
+                    if (game[laneNumber].estrada[i + 1] != 'O') {
+                        game[laneNumber].estrada[i] = ' ';
+                        game[laneNumber].estrada[i + 1] = 'C';
+                    }
+                    else {
+                        game[laneNumber].estrada[i] = ' ';
+                    }
+                }
+                else {
+                    game[laneNumber].estrada[i] = ' ';
+                }
+            }
+        }
+        // 25% de chance de spawnar um carro
+        if (*nCarros < 8) {
+            int flag = randNum(0, 4);
+            if (flag == 0 && game[laneNumber].estrada[0] != 'O') {
+                game[laneNumber].estrada[0] = 'C';
+            }
+        }
+    }
+}
--- Operador/Operador.c
+++ Operador/Operador.c
@@ -3,157 +3,56 @@
 #include <io.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include "Utils.h"
-//#include "Dll.h"
+//#include "Utils.h"
+#include "Game.h"
+#include "Dll.h"
 
 #define TAM_BUFFER 20
 #define TAM 100
-typedef struct {
-    //int id;
-    //int val;
-    TCHAR command[100];
-}BufferCell;
 
-//representa a nossa memoria partilhada
-typedef struct {
-    int posE; //proxima posicao de escrita
-    int posL; //proxima posicao de leitura
-    BufferCell buffer[TAM_BUFFER]; //buffer circular em si (array de estruturas)
-}SharedMem;
-
-
-//estrutura de apoio
-typedef struct {
-    SharedMem* memPar; //ponteiro para a memoria partilhada
-    HANDLE hSemEscrita; //handle para o semaforo que controla as escritas (controla quantas posicoes estao vazias)
-    HANDLE hSemLeitura; //handle para o semaforo que controla as leituras (controla quantas posicoes estao preenchidas)
-    HANDLE hMutex;
-    HANDLE hMapFile;
-    int terminar; // 1 para sair, 0 em caso contrário
-    int id;
-}ControlData;
-
-
-BOOL initMemAndSync(ControlData* dados) {
-    BOOL  primeiroProcesso = FALSE;
-
-    //criar semaforo que conta as escritas
-    dados->hSemEscrita = CreateSemaphore(NULL, TAM_BUFFER, TAM_BUFFER, TEXT("SO2_SEMAFORO_ESCRITA"));
-
-    //criar semaforo que conta as leituras
-    //0 porque nao ha nada para ser lido e depois podemos ir até um maximo de 10 posicoes para serem lidas
-    dados->hSemLeitura = CreateSemaphore(NULL, 0, TAM_BUFFER, TEXT("SO2_SEMAFORO_LEITURA"));
-
-    //criar mutex para os produtores
-    dados->hMutex = CreateMutex(NULL, FALSE, TEXT("SO2_MUTEX_PRODUTOR"));
-
-    if (dados->hSemEscrita == NULL || dados->hSemLeitura == NULL || dados->hMutex == NULL) {
-        _tprintf(TEXT("Erro no CreateSemaphore ou no CreateMutex\n"));
-        return -1;
-    }
-
-    //o openfilemapping vai abrir um filemapping com o nome que passamos no lpName
-    //se devolver um HANDLE ja existe e nao fazemos a inicializacao
-    //se devolver NULL nao existe e vamos fazer a inicializacao
-
-    dados->hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SO2_MEM_PARTILHADA"));
-    if (dados->hMapFile == NULL) {
-        primeiroProcesso = TRUE;
-        //criamos o bloco de memoria partilhada
-        dados->hMapFile = CreateFileMapping(
-            INVALID_HANDLE_VALUE,
-            NULL,
-            PAGE_READWRITE,
-            0,
-            sizeof(SharedMem), //tamanho da memoria partilhada
-            TEXT("TP_MEM_PARTILHADA"));//nome do filemapping. nome que vai ser usado para partilha entre processos
-
-        if (dados->hMapFile == NULL) {
-            _tprintf(TEXT("Erro no CreateFileMapping\n"));
-            return -1;
-        }
-    }
-
-    //mapeamos o bloco de memoria para o espaco de enderaçamento do nosso processo
-    dados->memPar = (SharedMem*)MapViewOfFile(dados->hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
-
-
-    if (dados->memPar == NULL) {
-        _tprintf(TEXT("Erro no MapViewOfFile\n"));
-        return -1;
-    }
-
-    if (primeiroProcesso == TRUE) {
-        dados->memPar->posE = 0;
-        dados->memPar->posL = 0;
-    }
-
-
-    dados->terminar = 0;
-
-    return TRUE;
-}
-
-DWORD WINAPI sendCommands(LPVOID param) {
-    ControlData* dados = (ControlData*)param;
-    BufferCell cel;
-    int contador = 0;
-
-    while (!dados->terminar) {
-        //ler uma frase
-        do {
-            _tprintf(TEXT("[OPERADOR] Commando: "));
-            _fgetts(cel.command, TAM_BUFFER, stdin);
-            cel.command[_tcslen(cel.command) - 1] = '\0';
-            _tprintf(TEXT("escreveu %s\n"), cel.command);
-            if (verifyCommand(cel.command) == 1)
-                break;
-        } while (1);
-
-        _tprintf(TEXT("escreveu %s\n"), cel.command);
-
-        //esperamos por uma posicao para escrevermos
-        WaitForSingleObject(dados->hSemEscrita, INFINITE);
-
-        //esperamos que o mutex esteja livre
+DWORD WINAPI ThreadLer(LPVOID param) {
+    ThreadDadosGame* dados = (ThreadDadosGame*)param;
+    while (1) {
+        //esperar até que evento desbloqueie
+        WaitForSingleObject(dados->hEvent, INFINITE);
+        //verifica se é preciso terminar a thread ou nao
+        if (dados->terminar)
+            break;
+        //faço o lock para o mutex
         WaitForSingleObject(dados->hMutex, INFINITE);
-
-        //vamos copiar a variavel cel para a memoria partilhada (para a posição de escrita)
-        CopyMemory(&dados->memPar->buffer[dados->memPar->posE], &cel, sizeof(BufferCell));
-        dados->memPar->posE++; //incrementamos a posicao de escrita para o proximo produtor escrever na posicao seguinte
-
-        //se apos o incremento a posicao de escrita chegar ao fim, tenho de voltar ao inicio
-        if (dados->memPar->posE == TAM_BUFFER)
-            dados->memPar->posE = 0;
-
-        //libertamos o mutex
+        //_tprintf(TEXT("Mensagem recebida: %s\n"), dados->sharedStruct->msg);
+        system("cls");
+        show(dados->sharedStruct->game);
+        
+        //faço unlock do mutex
         ReleaseMutex(dados->hMutex);
-
-        //libertamos o semaforo. temos de libertar uma posicao de leitura
-        ReleaseSemaphore(dados->hSemLeitura, 1, NULL);
-
-        contador++;
-        _tprintf(TEXT("P%d enviou o comando %s.\n"), dados->id, cel.command);
-        //Sleep(num_aleatorio(2, 4) * 1000);
+        //Sleep(1000);
     }
-    _tprintf(TEXT("P%d enviou %d comandos.\n"), dados->id, contador);
-
     return 0;
 }
 
 int _tmain(int argc, LPTSTR argv[]) {
-    HANDLE hFileMap; //handle para o file map
-    HANDLE hThread;
+    HANDLE hFileMap, hFileMapControl; //handle para o file map
+    HANDLE hThread[2];
     ControlData dados;
     TCHAR comando[100];;
+    ThreadDadosGame dadosGame;
+
 
 #ifdef UNICODE 
     _setmode(_fileno(stdin), _O_WTEXT);
     _setmode(_fileno(stdout), _O_WTEXT);
     _setmode(_fileno(stderr), _O_WTEXT);
 #endif
+    hFileMapControl = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "SO2_MEM_PART");
+    if (hFileMapControl == NULL) {
+        _tprintf(TEXT("[Erro] O Servidor ainda não foi iniciado!\n"));
+        
+        return 1;
+    }
     srand((unsigned int)time(NULL));
     initMemAndSync(&dados);
+    initMemAndSyncGame(&dadosGame);
 
 
     //temos de usar o mutex para aumentar o nProdutores para termos os ids corretos
@@ -164,15 +63,16 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 
     //lancamos a thread
-    hThread = CreateThread(NULL, 0, sendCommands, &dados, 0, NULL);
-    if (hThread != NULL) {
+    hThread[0] = CreateThread(NULL, 0, sendCommands, &dados, 0, NULL);
+    hThread[1] = CreateThread(NULL, 0, ThreadLer, &dadosGame, 0, NULL);
+    if (hThread[0] != NULL && hThread[1] != NULL) {
         /*
         _tprintf(TEXT("Escreva qualquer coisa para sair ...\n"));
         _getts_s(comando, 100);
         dados.terminar = 1;
         */
         //esperar que a thread termine
-        WaitForSingleObject(hThread, INFINITE);
+        WaitForMultipleObjects(2, hThread, TRUE, INFINITE);
     }
 
     UnmapViewOfFile(dados.memPar);
